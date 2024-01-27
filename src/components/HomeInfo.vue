92354fb3437a1de8906ac2f3b14d6b29fed7b94a
hessdale <hessdale96@gmail.com>
2024-01-26 22:26:58 -0800

    added rough layout of home page with over lapping images

--- src/components/HomeInfo.vue
+++ src/components/HomeInfo.vue
@@ -1,28 +1,40 @@
 <template>
     <div>
-        <div id="background">
-            <img id="openFridge" src="../assets/FridgeOpen.png" alt="fridge open with icons to click in it">
+        <div id="parent">
+            <img id="background" src="../assets/FridgeBackground.png" alt="" srcset="">
+            <img @click="openFridge()" id="openFridge" src="../assets/FridgeOpen.png"
+                alt="fridge open with icons to click in it" width="600px">
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    methods: {
+        openFridge() {
 
+        }
+    },
 }
 </script>
 
 <style scoped>
+#parent {
+    position: relative;
+}
+
 #background {
+    position: relative;
     width: 100%;
-    background-size: cover;
-    background-image: url(@/assets/FridgeBackground.png);
-    display: grid;
+    top: 0;
+    left: 0;
 }
 
 #openFridge {
-    width: 50%;
-    position: relative;
-    /* display: none; */
+    position: absolute;
+    width: 40%;
+    top: 21%;
+    left: 35.5%;
+    display: none;
 }
 </style>
