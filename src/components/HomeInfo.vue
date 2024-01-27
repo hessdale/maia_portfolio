7c27fd3eeb001c3509eb40a204b924db3c68f695
hessdale <hessdale96@gmail.com>
2024-01-26 22:12:00 -0800

    added home background and open background

--- src/components/HomeInfo.vue
+++ src/components/HomeInfo.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <img src="../assets/FridgeBackground.png" alt="a cute kitchen with a fridge that opens">
+        <div id="background">
+            <img id="openFridge" src="../assets/FridgeOpen.png" alt="fridge open with icons to click in it">
+        </div>
     </div>
 </template>
 
@@ -10,4 +12,17 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#background {
+    width: 100%;
+    background-size: cover;
+    background-image: url(@/assets/FridgeBackground.png);
+    display: grid;
+}
+
+#openFridge {
+    width: 50%;
+    position: relative;
+    /* display: none; */
+}
+</style>
