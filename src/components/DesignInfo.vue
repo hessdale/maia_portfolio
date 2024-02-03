8a755214de44fa4bb584c4f723ec4cbc72b91eb2
hessdale <hessdale96@gmail.com>
2024-02-02 20:48:21 -0800

    moved design data into its own component

---
+++ src/components/DesignInfo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+        <div id="background">
+            <article id="title">
+                <h1 class="whiteFont">design</h1>
+                <p class="whiteFont">Pieces In have designed for use accross print, web, and social media.</p>
+            </article>
+            <span id="images">
+                <img src="../assets/DesignAssets/arrow-left.svg" alt="icon left arrow">
+                <img src="../assets/DesignAssets/Bonsai_V01.jpg" alt="a photo of a bonsai tree" width="450px">
+                <img src="../assets/DesignAssets/arrow-right.svg" alt="icon right arrow">
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+#background {
+    background-color: #C72828;
+}
+
+.whiteFont {
+    color: #EAE5DE;
+}
+
+#images {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+}
+</style>
--- src/views/DesignView.vue
+++ src/views/DesignView.vue
@@ -1,41 +1,18 @@
 <template>
     <div>
         <nav-bar></nav-bar>
-        <div id="background">
-            <article id="title">
-                <h1 class="whiteFont">design</h1>
-                <p class="whiteFont">Pieces In have designed for use accross print, web, and social media.</p>
-            </article>
-            <span id="images">
-                <img src="../assets/DesignAssets/arrow-left.svg" alt="icon left arrow">
-                <img src="../assets/DesignAssets/Bonsai_V01.jpg" alt="a photo of a bonsai tree" width="450px">
-                <img src="../assets/DesignAssets/arrow-right.svg" alt="icon right arrow">
-            </span>
-        </div>
+        <design-info></design-info>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
+import DesignInfo from '@/components/DesignInfo.vue'
 export default {
     components: {
-        NavBar,
+        NavBar, DesignInfo
     },
 }
 </script>
 
-<style scoped>
-#background {
-    background-color: #C72828;
-}
-
-.whiteFont {
-    color: #EAE5DE;
-}
-
-#images {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
-}
-</style>
+<style scoped></style>
