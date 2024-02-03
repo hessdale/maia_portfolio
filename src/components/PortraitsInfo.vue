0052725abfc336cce012d67a88500d61f1744d8d
hessdale <hessdale96@gmail.com>
2024-02-02 20:53:06 -0800

    moved portraits page data to its own component

---
+++ src/components/PortraitsInfo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div id="background">
+            <article id="title">
+                <h1 class="whiteFont">portraits</h1>
+                <p class="whiteFont">Portraits I have taken commercially and creatively
+                </p>
+            </article>
+            <span id="images">
+                <img src="../assets/PortraitsAssets/arrow-left.svg" alt="icon left arrow">
+                <img src="../assets/PortraitsAssets/JewelryPortrait.jpeg" alt="portrait of a model wearing necklaces"
+                    width="450px">
+                <img src="../assets/PortraitsAssets/arrow-right.svg" alt="icon right arrow">
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+#background {
+    background-color: #C72828;
+}
+
+.whiteFont {
+    color: #EAE5DE;
+}
+
+#images {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+}
+</style>
--- src/views/PortraitsView.vue
+++ src/views/PortraitsView.vue
@@ -1,43 +1,18 @@
 <template>
     <div>
         <nav-bar></nav-bar>
-        <div id="background">
-            <article id="title">
-                <h1 class="whiteFont">portraits</h1>
-                <p class="whiteFont">Portraits I have taken commercially and creatively
-                </p>
-            </article>
-            <span id="images">
-                <img src="../assets/PortraitsAssets/arrow-left.svg" alt="icon left arrow">
-                <img src="../assets/PortraitsAssets/JewelryPortrait.jpeg" alt="portrait of a model wearing necklaces"
-                    width="450px">
-                <img src="../assets/PortraitsAssets/arrow-right.svg" alt="icon right arrow">
-            </span>
-        </div>
+        <portraits-info></portraits-info>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
+import PortraitsInfo from '@/components/PortraitsInfo.vue';
 export default {
     components: {
-        NavBar,
+        NavBar, PortraitsInfo
     },
 }
 </script>
 
-<style scoped>
-#background {
-    background-color: #C72828;
-}
-
-.whiteFont {
-    color: #EAE5DE;
-}
-
-#images {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
-}
-</style>
+<style scoped></style>
