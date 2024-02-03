9dff8b06b29a65f19af7c64c5c704bacbf468745
hessdale <hessdale96@gmail.com>
2024-02-02 20:50:44 -0800

    moved misc photos data to its own component

---
+++ src/components/PhotosInfo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div id="background">
+            <article id="title">
+                <h1 class="whiteFont">misc. photos</h1>
+                <p class="whiteFont">Images I have taken commercially and creatively of products, animals, bits, and bobs.
+                </p>
+            </article>
+            <span id="images">
+                <img src="../assets/MiscPhotosAssets/arrow-left.svg" alt="icon left arrow">
+                <img src="../assets/MiscPhotosAssets/Perfume.jpeg" alt="a photo of a bonsai tree" width="450px">
+                <img src="../assets/MiscPhotosAssets/arrow-right.svg" alt="icon right arrow">
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+#background {
+    background-color: #C72828;
+}
+
+.whiteFont {
+    color: #EAE5DE;
+}
+
+#images {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+}
+</style>
--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -1,42 +1,18 @@
 <template>
     <div>
         <nav-bar></nav-bar>
-        <div id="background">
-            <article id="title">
-                <h1 class="whiteFont">misc. photos</h1>
-                <p class="whiteFont">Images I have taken commercially and creatively of products, animals, bits, and bobs.
-                </p>
-            </article>
-            <span id="images">
-                <img src="../assets/MiscPhotosAssets/arrow-left.svg" alt="icon left arrow">
-                <img src="../assets/MiscPhotosAssets/Perfume.jpeg" alt="a photo of a bonsai tree" width="450px">
-                <img src="../assets/MiscPhotosAssets/arrow-right.svg" alt="icon right arrow">
-            </span>
-        </div>
+        <photos-info></photos-info>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
+import PhotosInfo from '@/components/PhotosInfo.vue';
 export default {
     components: {
-        NavBar,
+        NavBar, PhotosInfo
     },
 }
 </script>
 
-<style scoped>
-#background {
-    background-color: #C72828;
-}
-
-.whiteFont {
-    color: #EAE5DE;
-}
-
-#images {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
-}
-</style>
+<style scoped></style>
