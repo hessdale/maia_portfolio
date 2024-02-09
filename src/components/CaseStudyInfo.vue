572772870e0e784aadd61967b96ed929036f26ab
hessdale <hessdale96@gmail.com>
2024-02-08 21:38:01 -0800

    added rough lay out of blue ruby case study info at top of page and made a gif background

--- src/components/CaseStudyInfo.vue
+++ src/components/CaseStudyInfo.vue
@@ -6,9 +6,9 @@
             </span>
         </div>
         <span>
-            <div>
+            <div id="BlueRubyInfo">
                 <article>
-                    <h3>BLUE RUBY JEWELRY</h3>
+                    <h3 id="RubyInfoTitle">BLUE RUBY JEWELRY</h3>
                     <p>For over 25 years, Blue Ruby Jewellery has been
                         providing Vancouver with the largest selection of
                         both locally and globally designed jewellery. With
@@ -22,6 +22,34 @@
                         to create modern solutions that resonated with
                         their history and future goals.</p>
                 </article>
+                <div>
+                    <img src="../assets/BlueRubyAssets/Photography/Red_Shirt.jpg"
+                        alt="Woman wearing a red shirt and jewelry" width="200vw">
+                </div>
+            </div>
+        </span>
+        <span>
+            <div id="BlueRubyInfo2">
+                <div>
+                    <img src="../assets/BlueRubyAssets/Photography/Red_Shirt.jpg" width="200vw">
+                </div>
+                <article>
+                    <h3 class="underline">DELIVERABLES</h3>
+                    <p>- Packaging Design</p>
+                    <p>- Lifestyle Photography</p>
+                    <p>- Product Photography</p>
+                    <p>- Content Creation + Strategy</p>
+                    <p>- Newsletter Campaigns</p>
+                    <p>- Campaign Art Direction</p>
+                </article>
+                <article>
+                    <h3 class="underline">INDUSTRY</h3>
+                    <p>Ecommerce + Retail</p>
+                    <h3 class="underline">LOCATION</h3>
+                    <p>Vancouver,BC</p>
+                    <h3 class="underline">WEBSITE</h3>
+                    <a target="_blank" href="http://blueruby.com">Visit Site</a>
+                </article>
             </div>
         </span>
     </div>
@@ -34,6 +62,29 @@ export default {
 </script>
 
 <style scoped>
+.underline {
+    text-decoration: underline;
+}
+
+#BlueRubyInfo2 {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+
+#RubyInfoTitle {
+    text-decoration: underline;
+    font-family: 'NeueHaasUnicaPro-Black';
+}
+
+#BlueRubyInfo {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding-top: 20px;
+    text-align: start;
+}
+
 #Center {
     text-align: center;
     display: grid;
