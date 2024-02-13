692a891e7a356014f1f46c1687aa1c1849acf186
hessdale <hessdale96@gmail.com>
2024-02-13 09:50:44 -0800

    added simple login page that sends an axios request to db

---
+++ src/components/LoginInfo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <input type="text" placeholder="username" id="username">
+        <input type="text" placeholder="password" id="password">
+        <button @click="login">LOGIN</button>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+
+    methods: {
+        login() {
+            let username = this.$refs.username[`value`];
+            let password = this / this.$refs.password[`value`];
+            axios.request({
+                url: `${process.env.VUE_APP_BASE_URL}/api/client-login`,
+                method: `POST`,
+                data: {
+                    username: username,
+                    password: password,
+                },
+            }).then((response) => {
+                console.log(response)
+            }).catch((error) => {
+                console.log(error)
+                console.log("login failed")
+            })
+        }
+    },
+}
+</script>
+
+<style scoped></style>
