98b5095265b1b5f5b1072ec2ae2e373a570d8589
hessdale <hessdale96@gmail.com>
2024-02-20 16:11:06 -0800

    got about page to 90% just need to make it more dynamic for client

--- src/components/AboutInfo.vue
+++ src/components/AboutInfo.vue
@@ -2,27 +2,33 @@
     <div>
         <div id="about">
             <span id="who">
-                <img src="../assets/AboutAssets/MaiaKid.jpg" alt="a young photo of Maia giving the peace sign"
-                    width="200px">
-                <article>
-                    <h1 class="whiteFont" id="Title">Whose kid is this?</h1>
+                <div id="kid">
+                    <img src="../assets/AboutAssets/MaiaKid.jpg" alt="a young photo of Maia giving the peace sign">
+                </div>
+                <article id="whoKid">
+                    <h1 class="whiteFont" id="Bold">Whose kid is this?</h1>
                     <p class="whiteFont">This kid is me, and she (many years later) will work
                         with you to create process-driven, result-proven
                         work. Raised in Vancouver, BC, this kid grew to be
                         a designer and photographer with a big brain. Give
                         her a brief, and she’ll run with it.</p>
                 </article>
-                <article>
-                    <h1 class="whiteFont" id="Title">Not a Kid anymore.</h1>
+                <article id="not">
+                    <h1 class="whiteFont" id="Bold">Not a Kid anymore.</h1>
                     <p class="whiteFont">Today, this girl does it all! From photography to
                         illustrations, she’s a star. Looking for a commercial
                         photographer for your next project? Updated
                         branding for your business? She’s got you covered.</p>
                 </article>
-                <img src="../assets/AboutAssets/MaiaAdult.jpg" alt="a recent headshot of Maia" width="200px">
+                <div id="recent">
+                    <img src="../assets/AboutAssets/MaiaAdult.jpg" alt="a recent headshot of Maia">
+                </div>
             </span>
-            <a id="email" href="mailto:hadfieldmaia@gmail.com">hadfieldmaia@gmail.com</a>
-            <img src="../assets/AboutAssets/FloatingHeads.gif" alt="gif of floating heads of maia scrolling to the leaft">
+            <article id="contact">
+                <a id="email" href="mailto:hadfieldmaia@gmail.com">hadfieldmaia@gmail.com</a>
+                <img src="../assets/AboutAssets/FloatingHeads.gif"
+                    alt="gif of floating heads of maia scrolling to the leaft">
+            </article>
         </div>
     </div>
 </template>
@@ -34,18 +40,63 @@ export default {
 </script>
   
 <style scoped>
+#whoKid {
+    display: grid;
+    justify-items: center;
+    align-content: center;
+}
+
+#not {
+    display: grid;
+    justify-items: center;
+    align-content: center;
+}
+
+#contact {
+    display: grid;
+    grid-template-columns: 1fr;
+    justify-items: center;
+    padding-top: 5%;
+}
+
+#contact>img {
+    width: 100%;
+    padding-top: 5%;
+}
+
+#kid {
+    display: grid;
+    justify-items: center;
+}
+
+#kid>img {
+    width: 80%;
+}
+
+#recent {
+    display: grid;
+    justify-items: center;
+}
+
+#recent>img {
+    width: 75%;
+}
+
 #Bold {
     font-family: 'NeueHaasUnicaPro-Black';
+    margin: 0;
 }
 
 .whiteFont {
     color: #EAE5DE;
     text-align: start;
+    width: 80%;
 }
 
 #about {
     background-color: #C72828;
     height: 100vh;
+    padding-top: 3%;
 }
 
 #who {
@@ -57,5 +108,6 @@ export default {
 #email {
     text-decoration: none;
     color: #EAE5DE;
+    font-size: 20px;
 }
 </style>
