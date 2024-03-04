ec8dd0df483bf7aecc6d7d35d28961194e73e708
hessdale <hessdale96@gmail.com>
2024-03-03 19:42:11 -0800

    finished contat page to 95%

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -4,18 +4,36 @@
             <h1 class="whiteText">Let's make it happen!</h1>
             <p class="whiteText">Fill out this quick form or send an email and let’s
                 get talking. I look forward to working with you!</p>
-            <a href="mailto:hadfieldmaia@gmail.com">hadfieldmaia@gmail.com</a>
+            <a id="email" href="mailto:hadfieldmaia@gmail.com">hadfieldmaia@gmail.com</a>
         </div>
         <div id="form">
-            <input type="text ">
-            <p class="whiteText">First Name</p>
-            <input class="decoration" type="text">
-            <p class="whiteText">Last Name</p>
-            <input type="text">
-            <p class="whiteText">Email</p>
-            <input type="text">
-            <p class="whiteText">Note</p>
-            <button>Submit</button>
+            <div id="name">
+                <div>
+                    <input class="decoration" type="text" id="FirstNameInput">
+                    <p class="whiteText">First Name</p>
+                </div>
+                <div>
+                    <input class="decoration" type="text" id="LastNameInput">
+                    <p class="whiteText">Last Name</p>
+                </div>
+            </div>
+            <div id="email">
+                <div>
+                    <input class="decoration" type="text" id="emailInput">
+                    <p class="whiteText">Email</p>
+                </div>
+                <div id="empty">
+
+                </div>
+            </div>
+            <div id="note">
+                <input class="decoration" type="text" id="noteInput">
+                <p class="whiteText">Note</p>
+            </div>
+            <div id="button">
+                <button id="buttonDecoration">Submit</button>
+                <div></div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,11 +45,65 @@ export default {
 </script>
 
 <style scoped>
+#button {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+    align-items: center;
+    width: 650px;
+    margin-top: 20px;
+}
+
+#buttonDecoration {
+    text-decoration: none;
+    background-color: #C72828;
+    border: #EAE5DE;
+    border-width: 1px;
+    border-style: solid;
+    color: #EAE5DE;
+    width: 70px;
+    height: 30px;
+}
+
+#noteInput {
+    width: 380px;
+    height: 200px;
+}
+
+#email {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 1fr 1fr;
+}
+
+#name {
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-template-columns: 1fr 1fr;
+}
+
+#name>div {
+    margin: 10px;
+}
+
+#email>div {
+    margin: 10px;
+}
+
+#email {
+    text-decoration: none;
+    color: #EAE5DE;
+}
+
 .decoration {
     text-decoration: none;
     background-color: #C72828;
     border: #EAE5DE;
-    border-width: 2px;
+    border-width: 1px;
+    border-style: solid;
+    color: #EAE5DE;
 }
 
 #info {
