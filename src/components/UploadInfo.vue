b9cc97c9bd932ebaaa78a59c25f09a51172dc266
hessdale <hessdale96@gmail.com>
2024-03-21 12:20:41 -0700

    added axios request for uploading a design photo still need to work on the cookies verification

--- src/components/UploadInfo.vue
+++ src/components/UploadInfo.vue
@@ -3,17 +3,48 @@
         <input type="file" ref="uploaded_image">
         <input type="text" ref="image_description" placeholder="description of photo">
         <select name="photo type" id="type">
-            <option value="design">Design</option>
+            <option value="designs">Design</option>
             <option value="misc">Misc</option>
             <option value="portraits">Portraits</option>
         </select>
-        <button @click="addphoto">add photo</button>
+        <button @click="addPhoto">add photo</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import cookies from 'vue-cookies';
 export default {
+    methods: {
+        addPhoto() {
+            let photoType = document.getElementById("type").value;
+            let cookieToken = cookies.get("token")
+            if (photoType == "designs") {
+                let form = new FormData();
+                form.append("uploaded_image", this.$refs["uploaded_image"]["files"][0]);
+                form.append("image_description", this$refs["image_description"].value)
+                axios.request({
+                    url: `${process.env.VUE_APP_BASE_DOMAIN}/api/design-image`,
+                    method: `POST`,
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                        token: cookieToken
+                    },
+                    data: form,
+                }).then((response) => {
+                    console.log(response)
+                }).catch((error) => {
+                    console.log(error)
+                })
+            } if (photoType == "misc") {
 
+            } if (photoType == "portraits") {
+
+            } else {
+
+            }
+        }
+    },
 }
 </script>
 
