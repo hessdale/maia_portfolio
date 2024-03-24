d8d76bfde835d7b3528c919f4570b20bc929225e
hessdale <hessdale96@gmail.com>
2024-03-23 18:52:12 -0700

    working on client login process on db

--- src/components/LoginInfo.vue
+++ src/components/LoginInfo.vue
@@ -1,19 +1,20 @@
 <template>
     <div>
-        <input type="text" placeholder="username" id="username">
-        <input type="text" placeholder="password" id="password">
+        <input type="text" placeholder="username" ref="username">
+        <input type="text" placeholder="password" ref="password">
         <button @click="login">LOGIN</button>
     </div>
 </template>
 
 <script>
 import axios from "axios";
+import cookies from "vue-cookies";
 export default {
 
     methods: {
         login() {
             let username = this.$refs.username[`value`];
-            let password = this / this.$refs.password[`value`];
+            let password = this.$refs.password[`value`];
             axios.request({
                 url: `${process.env.VUE_APP_BASE_URL}/api/client-login`,
                 method: `POST`,
@@ -23,6 +24,7 @@ export default {
                 },
             }).then((response) => {
                 console.log(response)
+                cookies.set("token", response.data[0].token);
             }).catch((error) => {
                 console.log(error)
                 console.log("login failed")
