10eb2ad10375f3bac99928d0a9f07aa9a44ea316
hessdale <hessdale96@gmail.com>
2024-05-10 19:27:38 -0700

    did some styling on footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,10 +1,13 @@
 <template>
     <div id="info">
-        <h1 id="contact">contact</h1>
-        <div>
-            <a href="http://www.instagram.com"><img src="../assets/instagramLogo.svg"
-                    alt="icon of the instagram logo"></a>
-            <a href="http://www.behance.net"><img src="../assets/behance-tag.svg" alt="icon of the behance logo"></a>
+        <router-link class="link" to="/contact">
+            <h1 id="contact">contact</h1>
+        </router-link>
+        <div id="icons">
+            <a href="http://www.behance.net"><img src="../assets/behance-tag.svg" alt="icon of the behance logo"
+                    width="45px"></a>
+            <a href="http://www.instagram.com"><img src="../assets/instagramLogo.svg" alt="icon of the instagram logo"
+                    width="45px"></a>
         </div>
     </div>
 </template>
@@ -15,6 +18,14 @@ export default {
 </script>
 
 <style scoped>
+.link {
+    text-decoration: none;
+}
+
+#icons>a {
+    padding: 10px;
+}
+
 #info {
     display: grid;
     align-items: center;
