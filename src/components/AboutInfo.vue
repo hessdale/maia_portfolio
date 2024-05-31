6d36fabe7c2de36d94f69c78a08ae968746445c1
hessdale <hessdale96@gmail.com>
2024-05-31 16:34:59 -0700

    finished about page on most screen sizes

--- src/components/AboutInfo.vue
+++ src/components/AboutInfo.vue
@@ -2,8 +2,18 @@
     <div id="pageBackground">
         <div id="about">
             <span id="who">
+                <div class="mobile">
+                    <h1 class="whiteFont" id="Bold">Whose kid is this?</h1>
+                </div>
                 <div id="kid">
                     <img src="../assets/AboutAssets/MaiaKid.jpg" alt="a young photo of Maia giving the peace sign">
+                </div>
+                <div class="mobile">
+                    <p class="whiteFont">This kid is me, and she (many years later) will work
+                        with you to create process-driven, result-proven
+                        work. Raised in Vancouver, BC, this kid grew to be
+                        a designer and photographer with a big brain. Give
+                        her a brief, and she'll run with it.</p>
                 </div>
                 <article id="whoKid">
                     <h1 class="whiteFont" id="Bold">Whose kid is this?</h1>
@@ -20,8 +30,17 @@
                         photographer for your next project? Updated
                         branding for your business? She's got you covered.</p>
                 </article>
+                <div class="mobile">
+                    <h1 class="whiteFont" id="Bold">Not a kid anymore.</h1>
+                </div>
                 <div id="recent">
                     <img src="../assets/AboutAssets/MaiaAdult.jpg" alt="a recent headshot of Maia">
+                </div>
+                <div class="mobile">
+                    <p class="whiteFont">Today, this girl does it all! From photography to
+                        illustrations, she's a star. Looking for a commercial
+                        photographer for your next project? Updated
+                        branding for your business? She's got you covered.</p>
                 </div>
             </span>
             <article id="contact">
@@ -40,9 +59,12 @@ export default {
 </script>
 
 <style scoped>
+.mobile {
+    display: none;
+}
+
 #pageBackground {
     background-color: #C72828;
-    height: 100vh;
 }
 
 #whoKid {
@@ -66,6 +88,8 @@ export default {
 
 #contact>img {
     width: 100%;
+    padding-top: 20px;
+    padding-bottom: 40px;
 }
 
 #kid {
@@ -113,6 +137,48 @@ export default {
 #email {
     text-decoration: none;
     color: #EAE5DE;
-    font-size: 20px;
+    font-size: calc(100% + 2vw);
+}
+
+@media only screen and (max-width: 650px) {
+    #whoKid {
+        display: none;
+    }
+
+    #not {
+        display: none;
+    }
+
+    .mobile {
+        display: grid;
+        justify-items: center;
+        padding: 30px;
+    }
+
+    #who {
+        grid-template-columns: 1fr;
+    }
+
+    .whiteFont {
+        width: 100%;
+        text-align: center;
+        font-size: 25px;
+    }
+
+    #Bold {
+        font-size: calc(100% + 4vw)
+    }
+
+    #kid>img {
+        width: 100%;
+    }
+
+    #recent>img {
+        width: 100%;
+    }
+
+    #email {
+        font-size: 25px;
+    }
 }
 </style>
