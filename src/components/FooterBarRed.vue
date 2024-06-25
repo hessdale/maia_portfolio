8c25ed0ad5e986004dd897a33e46bbbadc3e32d9
hessdale <hessdale96@gmail.com>
2024-06-25 12:31:53 -0700

    added red footer bar component

---
+++ src/components/FooterBarRed.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="info">
+        <router-link class="link" to="/contact">
+            <h1 id="contact">contact</h1>
+        </router-link>
+        <div id="icons">
+            <a href="http://www.behance.net"><img src="../assets/NavAssets/behance-tag-beige.svg"
+                    alt="icon of the behance logo" width="45px"></a>
+            <a href="http://www.instagram.com"><img src="../assets/NavAssets/instagram-beige.svg"
+                    alt="icon of the instagram logo" width="45px"></a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.link {
+    text-decoration: none;
+}
+
+#icons>a {
+    padding: 10px;
+}
+
+#info {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    background-color: #C72828;
+    padding: 30px;
+}
+
+#contact {
+    font-family: 'NeueHaasUnicaPro-Black';
+    color: #EAE5DE;
+}
+</style>
--- src/views/GoodCaseStudyView.vue
+++ src/views/GoodCaseStudyView.vue
@@ -2,17 +2,17 @@
     <div>
         <nav-bar></nav-bar>
         <good-grub-case-study-info></good-grub-case-study-info>
-        <footer-bar></footer-bar>
+        <footer-bar-red></footer-bar-red>
     </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
 import GoodGrubCaseStudyInfo from '../components/GoodGrubCaseStudyInfo.vue';
-import FooterBar from '@/components/FooterBar.vue';
+import FooterBarRed from '@/components/FooterBarRed.vue';
 export default {
     components: {
-        GoodGrubCaseStudyInfo, NavBar, FooterBar
+        GoodGrubCaseStudyInfo, NavBar, FooterBarRed
     },
 }
 </script>
